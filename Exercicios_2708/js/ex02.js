2760a06f036d964a33ffb96391f4fd5bf40f1771
alicebiaurnau <aliceurnau@gmail.com>
2025-08-27 16:14:17 -0300

    Ex02 e Lista completa
    numero menor que tres vezes dois e número maior que tres vezes tres

---
+++ Exercicios_2708/js/ex02.js
@@ -0,0 +1,12 @@
+var n, result
+
+n=parseInt(prompt("Digite um número: "))
+
+if(n<=3){
+    result=n*2
+    alert("O triplo do número é "+result)
+}
+else{
+    result=n*3
+    alert("O triplo do número é "+result)
+}
