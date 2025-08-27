5155886dddd02009d5990cf49a3b78dcf0bfb97d
alicebiaurnau <aliceurnau@gmail.com>
2025-08-27 16:02:40 -0300

    Ex 01
    número menor ou igual a 3

---
+++ Exercicios_2708/js/ex01.js
@@ -0,0 +1,11 @@
+var n, result
+
+n=parseInt(prompt("Digite um número: "))
+
+if(n<=3){
+    result=n*3
+    alert("O triplo do número é "+result)
+}
+else{
+    alert("O número digitado é maior que três. Programa invalidado")
+}
